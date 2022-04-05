dc3b66ae463d997a4d6897793bfa001020c2023d
jbmahaffey <84487961+jbmahaffey@users.noreply.github.com>
2022-04-05 15:49:36 -0700

    Update faz-logsearch.py

--- faz-logsearch.py
+++ faz-logsearch.py
@@ -18,8 +18,6 @@ def main():
     parser.add_argument('--fortianalyzer', default='jmahaffey-api-test.fortidemo.fortinet.com:10405', help='FortiAnalyzer IP Address')
     parser.add_argument('--user', default='', help='FAZ API User')
     parser.add_argument('--password', default='', help='FAZ API User Password')
-    parser.add_argument('--logging', default='', help='Logging levels info, error, or debug')
-    parser.add_argument('--devlist', default='address.csv', help='YAML/CSV file with list of approved devices.')
     args = parser.parse_args()
 
     url = 'https://%s/jsonrpc' % args.fortianalyzer
